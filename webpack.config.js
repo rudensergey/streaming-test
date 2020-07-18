ceff58c1331fd22bb1ad8699118e7957dd4ce2af
rudensergey <rudensergey@gmail.com>
2020-07-18 13:45:40 +0300

    set webpack

--- webpack.config.js
+++ webpack.config.js
@@ -1,9 +1,18 @@
 const path = require('path');
+const HtmlWbpackPlugin = require('html-webpack-plugin');
+
+const is_prod = process.env.NODE_ENV;
 
 module.exports = {
+	mode: is_prod === 'production',
     entry: './src/frodo.js',
     output: {
         path: path.resolve(__dirname, 'build'),
-        filename: '[hashname].js',
+        filename: '[hash].js',
     },
+    plugins: [
+        new HtmlWbpackPlugin({
+            template: './src/index.html',
+        }),
+    ],
 };
